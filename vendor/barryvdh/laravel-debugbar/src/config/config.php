b556cea710c0bd5e7034c0f7b110b33eea7721f1
David Carrithers <dcarrith@gmail.com>
2013-10-29 10:03:40 -0400

    Adding in other vendor libraries that were missed because of an entry in gitignore

---
+++ vendor/barryvdh/laravel-debugbar/src/config/config.php
@@ -0,0 +1,69 @@
+<?php
+
+return array(
+
+
+ /*
+  |--------------------------------------------------------------------------
+  | Debugbar Settings
+  |--------------------------------------------------------------------------
+  |
+  | Debugbar is enabled by default, when debug is set to true in app.php.
+  |
+  */
+  'enabled' => \Config::get('app.debug'),
+
+  /*
+   |--------------------------------------------------------------------------
+   | Vendors
+   |--------------------------------------------------------------------------
+   |
+   | Vendor files are included by default, but can be set to false.
+   | This can also be set to 'js' or 'css', to only include javascript or css vendor files.
+   | Vendor files are for css: font-awesome (including fonts) en for js: jquery 1.8.3
+   |
+   */
+  'include_vendors' => true,
+
+  /*
+   |--------------------------------------------------------------------------
+   | Capture Ajax Requests
+   |--------------------------------------------------------------------------
+   |
+   | The Debugbar can capture Ajax requests and display them. If you don't want this (ie. because of errors),
+   | you can use this option to disable sending the data through the headers.
+   |
+   */
+  'capture_ajax' => true,
+
+
+ /*
+  |--------------------------------------------------------------------------
+  | Event logging
+  |--------------------------------------------------------------------------
+  |
+  | Enable/disable DataCollectors
+  |
+  */
+   'collectors' => array(
+       'phpinfo' => true,           // Php version
+       'messages' => true,          // Messages
+       'time' => true,              // Time Datalogger
+       'memory' => true,            // Memory usage
+       'exceptions' => true,        // Exception displayer
+       'log' => true,               // Logs from Monolog (merged in messages if enabled)
+       'db' => true,                // Show database (PDO) queries and bindings
+       'views' => true,             // Views with their data
+       'route' => true,             // Current route information
+       'laravel' => false,          // Laravel version and environment
+       'events' => false,           // All events fired
+       'twig' => false,             // Twig, requires barryvdh/laravel-twigbridge
+       'default_request'=> false,   // Regular or special Symfony request logger
+       'symfony_request'=> true,    // Only one can be enabled..
+       'mail' => true,              // Catch mail messages
+       'mail_log' => false,         // Display full mail log in messages
+
+   )
+
+
+);
---
+++ vendor/barryvdh/laravel-ide-helper/src/config/config.php
@@ -0,0 +1,60 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filename
+    |--------------------------------------------------------------------------
+    |
+    | The default path to the helper file
+    |
+    */
+
+    'filename' => '_ide_helper.php',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Helper files to include
+    |--------------------------------------------------------------------------
+    |
+    | Include helper files. By default not included, but can be toggled with the
+    | -- helpers (-H) option. Extra helper files can be included.
+    |
+    */
+
+    'include_helpers' => false,
+
+    'helper_files' => array(
+        base_path().'/vendor/laravel/framework/src/Illuminate/Support/helpers.php',
+    ),
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Extra classes
+    |--------------------------------------------------------------------------
+    |
+    | These implementations are not really extended, but called with magic functions
+    |
+    */
+
+    'extra' => array(
+        'Eloquent'  => array('Illuminate\Database\Eloquent\Builder', 'Illuminate\Database\Query\Builder'),
+        'Session' => array('Illuminate\Session\Store'),
+    ),
+
+    'magic' => array(
+        'Log' => array(
+            'debug'     => 'Monolog\Logger::addDebug',
+            'info'      => 'Monolog\Logger::addInfo',
+            'notice'    => 'Monolog\Logger::addNotice',
+            'warning'   => 'Monolog\Logger::addWarning',
+            'error'     => 'Monolog\Logger::addError',
+            'critical'  => 'Monolog\Logger::addCritical',
+            'alert'     => 'Monolog\Logger::addAlert',
+            'emergency' => 'Monolog\Logger::addEmergency',
+        )
+    )
+
+);
