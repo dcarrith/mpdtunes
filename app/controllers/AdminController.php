082c0dde3b58a0e43752b0587658cfc5e4ba2dc8
David Carrithers <dcarrith@gmail.com>
2014-02-06 10:59:22 -0500

    Cleaned up the admin controller by removing a bunch of debugging firephp calls as well as some code that wasn't used anymore

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -15,10 +15,6 @@ class AdminController extends MPDTunesController {
 		// home doesn't need to be ssl encrypted
 		$home_link = $this->data['base_protocol'] . $this->data['base_domain'] . "/";
 
-		$this->firephp->log($this->data['base_protocol'], "base_protocol");
-		$this->firephp->log($this->data['base_domain'], "base_domain");
-		$this->firephp->log($home_link, "home_link");
-
 		// we have to override the default so that it links back to home properly
 		$this->data['home_link_data_ajax']	= "false";
 		$this->data['home_link']		= $home_link;
@@ -52,13 +48,6 @@ class AdminController extends MPDTunesController {
                 $password       	= Input::old('password');
 		$password_confirmation 	= Input::old('password_confirmation');
 
-                $this->firephp->log($user_id, "user id");
-                $this->firephp->log($first_name, "first name");
-                $this->firephp->log($last_name, "last name");
-                $this->firephp->log($email, "email");
-                $this->firephp->log($password, "password");
-		$this->firephp->log($password_confirmation, "password_confirmation");
-			
 		$this->data['user_id']	= $user_id;
 		$this->data['first_name'] = $first_name;
 		$this->data['last_name'] = $last_name;
@@ -71,19 +60,12 @@ class AdminController extends MPDTunesController {
 
                 if (Session::has('success')) {
 
-                        $this->firephp->log(Session::get('success'), 'successful?');
-
                         if (Session::get('success')) {
 
                                 $this->data['saved_successfully'] = true;
 
 				$user = User::find($user_id);
-                                $this->firephp->log($user, 'user');
-                                $this->firephp->log($user->first_name, 'first_name');
-                                $this->firephp->log($user->last_name, 'last_name');
-                                $this->firephp->log($user->email, 'username');
-                                $this->firephp->log($user->password, 'password');
-                          
+
                         } else {
 
                                 $this->data['error_occurred_while_saving'] = true;
@@ -101,12 +83,7 @@ class AdminController extends MPDTunesController {
 			$this->data['username'] = $user->email;
 	
 			$this->data['password'] = $password;
-			$this->data['password_confirmation'] = $password_confirmation;
-	
-			$this->firephp->log($user, 'user');
-			$this->firephp->log($user->first_name, 'first_name');
-			$this->firephp->log($user->last_name, 'last_name');
-			$this->firephp->log($user->email, 'username');
+			$this->data['password_confirmation'] = $password_confirmation;	
 		}
 
                 return View::make('account', $this->data);
@@ -120,22 +97,11 @@ class AdminController extends MPDTunesController {
 		$email          	= Request::get('username');
 		$password       	= Request::get('password');
 		$password_confirmation 	= Request::get('password_confirmation');
-
-		$this->firephp->log($user_id, "user_id");
-		$this->firephp->log($first_name, "first name");
-		$this->firephp->log($last_name, "last name");
-		$this->firephp->log($email, "username");
-		$this->firephp->log($password, "password");
-		$this->firephp->log($password_confirmation, "password_confirmation");
-
-		//$this->load->library('user_agent');
-		//$this->firephp->log($this->agent->referrer(), "this->agent->referrer()");
-
+		
 		// Add the csrf before filter to guard against cross-site request forgery
 		$this->beforeFilter('csrf');
                 
 		try {
-
 			$validate = new UserValidationService(Input::all());
 
 			$validate->account();
@@ -160,8 +126,6 @@ class AdminController extends MPDTunesController {
 
 			Session::flash('success', true);
 
-			$this->firephp->log($success, "success");
-
 			$this->data['saved_successfully'] = true;
 
 			return Redirect::to('admin/account')->withInput();
@@ -169,8 +133,6 @@ class AdminController extends MPDTunesController {
 		} else {
 
 			Session::flash('success', false);
-			
-			$this->firephp->log($success, "success");
 
 			return Redirect::to('admin/account')->withInput();
 		}
@@ -198,8 +160,6 @@ class AdminController extends MPDTunesController {
 
 		$usersConfigs = $user->usersConfig->config();
 
-		$this->firephp->log($usersConfigs, 'usersConfigs');
-
 		// First use the value from the active field of the user's database record to detetermine on or off
 		$user_account_active_on_off = (($user->active) ? 'on' : 'off');
 
@@ -223,12 +183,9 @@ class AdminController extends MPDTunesController {
 		// If the old input value for user_account_active is set, then use it, otherwise use the value retrieved above
 		$user_account_active_on_off = Input::old('user_account_active', $user_account_active_on_off);
 
-		$this->firephp->log($user_account_active_on_off, "user_account_active_on_off");
-
 		if (!isset($user_account_active_on_off)) {
 
 			$user_account_active_on_off = "off";
-			$this->firephp->log($user_account_active_on_off, "user_account_active_on_off");
 		}
 
                 // We need to set up the options to pass to the Form::input for the user account active checkbox
@@ -246,9 +203,7 @@ class AdminController extends MPDTunesController {
 
 			$this->data['user_account_active'] = 'off';
                 }
-		
-		$this->firephp->log($this->data, "this->data");
-				
+						
 		return View::make('user', $this->data);
 	}
 
@@ -268,19 +223,6 @@ class AdminController extends MPDTunesController {
 		$posted_user_account_active 		= Request::get('user_account_active');
 
                 $user_account_active = (($posted_user_account_active == "on") ? 1 : 0);
-
-		$this->firephp->log($posted_user_id, 'posted_user_id');
-		$this->firephp->log($posted_mpd_host, 'posted_mpd_host');
-		$this->firephp->log($posted_mpd_port, 'posted_mpd_port');
-		$this->firephp->log($posted_mpd_stream_port, 'posted_mpd_stream_port');
-		$this->firephp->log($posted_mpd_password, 'posted_mpd_password');
-		$this->firephp->log($posted_mpd_password_confirmation, 'posted_mpd_password_confirmation');
-		$this->firephp->log($posted_mpd_dir, 'posted_mpd_dir');
-		$this->firephp->log($posted_music_dir, 'posted_music_dir');
-		$this->firephp->log($posted_queue_dir, 'posted_queue_dir');
-		$this->firephp->log($posted_art_dir, 'posted_art_dir');
-		$this->firephp->log($posted_user_account_active, 'posted_user_account_active');
-                $this->firephp->log($user_account_active, "user_account_active");
 
                 // Add the csrf before filter to guard against cross-site request forgery
                 $this->beforeFilter('csrf');
@@ -303,8 +245,6 @@ class AdminController extends MPDTunesController {
 
 		if ($success) {
 
-			$this->firephp->log($posted_user_id, "posted_user_id");
-
 			$user_configs = array();
 			$user_configs['mpd']['mpd_host']	= $posted_mpd_host;
 			$user_configs['mpd']['mpd_port'] 	= $posted_mpd_port;
@@ -319,15 +259,11 @@ class AdminController extends MPDTunesController {
 			// Retrieve the UsersConfig object associated with the posted_user_id
 			$usersConfig = UsersConfig::find($posted_user_id);
                         	
-			// Create, compress, encrypt and convert the binary data to a bin hex for MySQL blob format 
-			//$usersConfig->config = create_compress_encrypt_config($user_configs, 'user', $this->zipper, $this->firephp);
-			
+			// Serialize and then encrypt the users config	
 			$usersConfig->config = Crypt::encrypt( serialize( $user_configs ));
 	
 			// Save the updated usersConfig object
 			$success = $usersConfig->save();
-
-			$this->firephp->log($success, 'success');	
 
 			if ( $success ) {
 
@@ -539,139 +475,77 @@ class AdminController extends MPDTunesController {
 
 				if ($user_id > 1) {
 
-				$user = User::find($user_id);
+					$user = User::find($user_id);
 
-				$usersConfig = $user->usersConfig;
+					$usersConfig = $user->usersConfig;
 
-				$usersConfigs = $usersConfig->config();
+					$usersConfigs = $usersConfig->config();
 
-				// Delete the user's config record
-				$usersConfig->delete();
+					// Delete the user's config record
+					$usersConfig->delete();
 
-				$this->firephp->log($usersConfigs, 'usersConfigs');
-
-                                $mpd_dir    = $usersConfigs['mpd']['mpd_dir'];
-                                $music_dir  = $usersConfigs['music']['music_dir'];
-                                $queue_dir  = $usersConfigs['music']['queue_dir'];
-                                $art_dir    = $usersConfigs['music']['art_dir'];
-
-                                $this->firephp->log($mpd_dir, "mpd_dir");
-                                $this->firephp->log($music_dir, "music_dir");
-                                $this->firephp->log($queue_dir, "queue_dir");
-                                $this->firephp->log($art_dir, "art_dir");
-
-                                $user_directories_destroyed = destroy_user_directories($mpd_dir, $music_dir, $queue_dir, $art_dir, $firephp);
-
-                                $this->firephp->log($user_directories_destroyed, "user_directories_destroyed");
-
-            			$station = $user->station;                    
-
-        			$this->firephp->log($station->id, "station id");
-        			$this->firephp->log($station->name, "station name");
-        			$this->firephp->log($station->description, "station description");
-        			$this->firephp->log($station->url, "station url");
-        			$this->firephp->log($station->icon_id, "station icon_id");
-
-				$stationIcon = $station->stationsIcon;
-
-				if ($stationIcon->id > 1) {
-
-					$this->firephp->log("Deleting the station icon since it was a custom icon uploaded by the user that is being deleted");
-					$stationIcon->delete();
-				}
-
-				$this->firephp->log($stationIcon, "stationIcon");
-        			$this->firephp->log($stationIcon->id, "icon id");
-        			$this->firephp->log($stationIcon->filepath, "icon filepath");
-        			$this->firephp->log($stationIcon->filename, "icon filename");
-
-				$owner = $station->owner;
-				
-				$this->firephp->log($owner, "owner");
-				$this->firephp->log($owner->id, "owner id");
-				$this->firephp->log($owner->email, "owner email");
-
-				$this->firephp->log($station->name, "The following users station is being deleted");
-				$station->delete();
-
-				$stationIcons = $user->stationIcons;
-
-				foreach($stationIcons as $stationIcon) {
-
-					$this->firephp->log($stationIcon->id, "stationIcon id");
-        				$this->firephp->log($stationIcon->filename, "stationIcon filename");
-        				$this->firephp->log($stationIcon->filepath, "stationIcon filepath");
-				
-					if ($stationIcon->id > 1) {
-						
-						$this->firephp->log("The stationIcon can now be deleted.", "message");
-						$stationIcon->delete();
-					}
-				}
-
-				$station = null;
-
-            			$stations = $user->stations;                    
-
-				$this->firephp->log($stations->toJson(), "users stations");
+                                	$mpd_dir    = $usersConfigs['mpd']['mpd_dir'];
+                                	$music_dir  = $usersConfigs['music']['music_dir'];
+                                	$queue_dir  = $usersConfigs['music']['queue_dir'];
+                                	$art_dir    = $usersConfigs['music']['art_dir'];
 	
-				foreach($stations as $station) {
-				
-					$this->firephp->log($station->id, "station id");
-        				$this->firephp->log($station->name, "station name");
-        				$this->firephp->log($station->description, "station description");
-        				$this->firephp->log($station->url, "station url");
-        				$this->firephp->log($station->icon_id, "station icon_id");
+        	                        $user_directories_destroyed = destroy_user_directories($mpd_dir, $music_dir, $queue_dir, $art_dir, $firephp);
+
+            				$station = $user->station;                    
 
 					$stationIcon = $station->stationsIcon;
-	
-					$this->firephp->log($stationIcon->id, "Deleting stationIcon with id");
-					// Delete the stationIcon for the station
-					$stationIcon->delete();
 
-					$this->firephp->log($stationIcon, "stationIcon");
-        				$this->firephp->log($stationIcon->id, "icon id");
-        				$this->firephp->log($stationIcon->filepath, "icon filepath");
-        				$this->firephp->log($stationIcon->filename, "icon filename");
+					if ($stationIcon->id > 1) {
+
+						$this->firephp->log("Deleting the station icon since it was a custom icon uploaded by the user that is being deleted");
+						$stationIcon->delete();
+					}
 
 					$owner = $station->owner;
-					
-					$this->firephp->log($owner, "owner");
-					$this->firephp->log($owner->id, "owner id");
-					$this->firephp->log($owner->email, "owner email");
 
-					$creator = $station->creator;
+					$station->delete();
+
+					$stationIcons = $user->stationIcons;
+
+					foreach($stationIcons as $stationIcon) {
+
+						if ($stationIcon->id > 1) {
+						
+							$stationIcon->delete();
+						}
+					}
+
+					$station = null;
+
+            				$stations = $user->stations;                    
+	
+					foreach($stations as $station) {
 				
-					$this->firephp->log($creator, "creator");
-					$this->firephp->log($creator->id, "creator id");
-					$this->firephp->log($creator->email, "creator email");
+						$stationIcon = $station->stationsIcon;
+	
+						// Delete the stationIcon for the station
+						$stationIcon->delete();
+						
+						$owner = $station->owner;
+	
+						$creator = $station->creator;
+				
+						// Delete this station that was added by the user
+						$station->delete();	
+					}	
 
-					$this->firephp->log($station->id, "Deleting station with id");
-					// Delete this station that was added by the user
-					$station->delete();	
-				}	
+					$preferences = $user->preferences;
 
-				$preferences = $user->preferences;
+					// Delete the user's preferences
+					$preferences->delete();
+	
+					$themes = $user->themes;
 
-				$this->firephp->log($preferences, "users preferences");
-				$this->firephp->log($preferences->user_id, "preferences user_id");
-				$this->firephp->log($preferences->theme_id, "preferences theme_id");	
-				$this->firephp->log($preferences->mode, "preferences mode");
-				$this->firephp->log($preferences->crossfade, "preferences crossfade");
-				$this->firephp->log($preferences->volume_fade, "preferences volume_fade");
-				$this->firephp->log($preferences->language_id, "preferences language_id");
+					foreach($themes as $theme) {
 
-				// Delete the user's preferences
-				$preferences->delete();
-
-				$themes = $user->themes;
-
-				foreach($themes as $theme) {
-
-					$this->firephp->log($theme->id, "Deleting theme with id");
-					// Delete this theme that was added by the user
-					$theme->delete();	
-				}
+						// Delete this theme that was added by the user
+						$theme->delete();	
+					}
 
 				} else {
 	
@@ -680,40 +554,7 @@ class AdminController extends MPDTunesController {
 
 				// Now finally delete the user record
 				$user->delete();
-
-				exit();
-
-                                require_once('includes/php/library/stations.inc.php');
-
-                                // get the user's personal station
-                                $users_station = get_users_station($this->db, $user_id);
-
-                                $users_station_id = $users_station['id'];
-
-                                $this->firephp->log($users_station, "users_station");
-
-                                require_once('includes/php/library/users.inc.php');
-
-                                $station_deleted = delete_station($this->db, $users_station_id);
-        
-                                $this->firephp->log($station_deleted, "station_deleted");
-
-                                require_once('includes/php/library/settings.inc.php');
-
-                                $user_preferences_deleted = delete_user_preferences($this->db, $user_id);
-
-                                $this->firephp->log($user_preferences_deleted, "user_preferences_deleted");
-
-                                $user_deleted = delete_user($this->db, $user_id, $this->firephp);
-                                
-                                $this->firephp->log($user_deleted, "user_deleted");
-                                
-                                if ($user_directories_destroyed && $station_deleted && $user_preferences_deleted && $user_deleted) {
-                                
-                                        $this->firephp->log($user_directories_destroyed, "returning true");
-                                        return true;
-                                }
-                        }
+    			}
                 }
 
                 return false;
